4c30313297b57aa737944b7b31a7a36df0c21679
ChaseReineke <chase.e.reineke@gmail.com>
2021-01-12 23:46:43 -0800

    initial commit

---
+++ ProjectTest.java
@@ -0,0 +1,7 @@
+
+public class ProjectTest extends Project{
+    public static void main(String[] args){
+        Project proj = new Project("Test", "A test project");
+        System.out.println(proj.elevatorPitch());
+    }
+}
